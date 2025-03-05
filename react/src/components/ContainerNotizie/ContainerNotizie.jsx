22d85ba2e6d84a059b1367523eb3379af4a6ab2c
Francesco <francesco.bocci09@gmail.com>
2025-03-05 16:27:28 +0100

    re

--- react/src/components/ContainerNotizie/ContainerNotizie.jsx
+++ react/src/components/ContainerNotizie/ContainerNotizie.jsx
@@ -1,11 +1,14 @@
 import "./ContainerNotizie.css"
 
-export function ContainerNotizie()
+export function ContainerNotizie(props)
 {
     return(
         <div className="container-notizia">
             <img className="immagine-notizia" src="https://st2.depositphotos.com/6789684/12262/v/450/depositphotos_122620866-stock-illustration-illustration-of-flat-icon.jpg"  alt ="Immagine mancante"></img>
-            <text className="titolo-notizia"> Esempio</text>
+            <div className="text-box">
+                <text className="titolo-notizia" alt = "titolo"> {props.titolo} </text>
+                <text className="descrizione-notizia" alt = "descrizione "> {props.descrizione}  </text>
+            </div>
         </div>
     );
 }
--- react/src/pages/Notizie/Notizie.jsx
+++ react/src/pages/Notizie/Notizie.jsx
@@ -1,10 +1,11 @@
 import { ContainerNotizie } from "../../components/ContainerNotizie/ContainerNotizie";
 import "./Notizie.css"
 export function Notizie(){
+    
     return(
-        <div className="Notizie">
-        <h1 className="Title">Notizie</h1>
-        <ContainerNotizie/>
+        <div className="notizie">
+        <h1 className="titolo">Notizie</h1>
+        <ContainerNotizie className = "Container" titolo = "titolo" descrizione = "descrizione"/>
         </div>
     );
 }
