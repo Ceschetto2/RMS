7132e6b58cbf8e0b8d516a58b1166f81b46e5d8c
Francesco <francesco.bocci09@gmail.com>
2025-03-10 18:25:09 +0100

    Aggiornata la navbar e aggiunte le routes per le pagine della home

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,16 @@
 import { Home } from './pages/Home/Home';
 import {Notizie} from './pages/Notizie/Notizie';
+import { Bandi } from './pages/Bandi/Bandi';
+import { FAQ } from './pages/FAQ/FAQ';
+import { Regolamento } from './pages/Regolamento/Regolamento';
+import { Gallery } from './pages/Gallery/Gallery';
+
 import { Layout } from './Layout';
 
 import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+
+
 
 function App() {
 
@@ -15,6 +22,11 @@ function App() {
         <Route element = {<Layout/>}>
           <Route path="/" element ={<Home/>} />  
           <Route path="/notizie" element ={<Notizie/>} /> 
+          <Route path="/bandi" element ={<Bandi/>} /> 
+          <Route path="/FAQ" element ={<FAQ/>} /> 
+          <Route path="/regolamento" element ={<Regolamento/>} /> 
+          <Route path="/gallery" element ={<Gallery/>} />
+
         </Route> 
       </Routes>
     </Router>
--- react/src/components/Navbar/Navbar.jsx
+++ react/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,29 @@ export function Navbar() {
   return (
     <div className="navbar">
       <div className="nav-link-list">
-        <Link className="nav-link" to="/">
-          <button className="nav-button">Home</button>
+        <Link className="nav-link-image" to="/">
+          <img className="nav-image"  src="https://iili.io/3fDhbyb.png" alt="home"></img>
+        </Link>
+   
+        <Link className="nav-link" to="/gallery">
+          <button className="nav-button">Gallery</button>
         </Link>
 
         <Link className="nav-link" to="/notizie">
           <button className="nav-button">Notizie</button>
         </Link>
+
+        <Link className="nav-link" to="/bandi">
+          <button className="nav-button">Bandi</button>
+        </Link>
+
+        <Link className="nav-link" to="/regolamento">
+          <button className="nav-button">Regolamento</button>
+        </Link>
+        <Link className="nav-link" to="/FAQ">
+          <button className="nav-button">FAQ</button>
+        </Link>
+        
       </div>
     </div>
   );
