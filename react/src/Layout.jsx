e0ccee0df04bea209378a9531512c20ca6608efe
Francesco <francesco.bocci09@gmail.com>
2025-03-13 13:26:14 +0100

    aggiunto un contenitore flex che contiene tutti gli elementi della pagina dalla navbar al footer

--- react/src/Layout.jsx
+++ react/src/Layout.jsx
@@ -1,11 +1,11 @@
 import { Downbar } from "./components/DownBar/Downbar";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
-
+import "./Layout.css"
 
 export function Layout() {
   return (
-    <>
+    <div className="background-pages">
       <Navbar />
 
       <main>
@@ -13,6 +13,6 @@ export function Layout() {
       </main>
 
       <Downbar/>
-    </>
+    </div>
   );
 }
