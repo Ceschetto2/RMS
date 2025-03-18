a951d8fba6b1416a4b0bf00b0ec592628e277f3d
Francesco <francesco.bocci09@gmail.com>
2025-03-18 20:52:20 +0100

    creato il componente che permette la visualizzazione di immagini a carosello. Da migliorare

--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
+import { ImgSlideShow } from "../../components/ImgSlideShow/ImgSlideShow";
 import "./Home.css"
 export function Home(){
+      let img_array = [
+        "https://placehold.co/400?text=ciao",
+        "https://placehold.co/400?text=bellezze",
+        "https://placehold.co/400?text= trapani in fiamme",
+      ];
     return(
+        
         <>
+        <ImgSlideShow img_array={img_array}/>
         </>
     );
 }
