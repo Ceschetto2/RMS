000c90e9071de3e3a9427897c2b03dd4fa0d39e2
Francesco <francesco.bocci09@gmail.com>
2025-03-19 17:30:16 +0100

    Aggiunta una versione alpha della gallery

--- react/src/pages/Gallery/Gallery.jsx
+++ react/src/pages/Gallery/Gallery.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
+import "./Gallery.css";
+import { PageTitle } from "../../components/PageTitle/PageTitle";
+import { ImgSlideShow } from "../../components/ImgSlideShow/ImgSlideShow";
+import { ImgGallery } from "../../components/ImgGallery/ImgGallery";
 
+
+
+/*rendere i bottone evidenziato in base alla scelta selezionata, bisogna far 
+variare il css in base allo stato della variabile img_show
+Da aggiungere un paging system per la vista a gallery o un caricamento dinamico delle immagini
+per lo slideshow
+*/
 
 export function Gallery()
 {
-    return(
-        <>
-        </>
+    let titolo_info = {
+        titolo: "Gallery",
+        search_bar_default_text: "test",
+        search_buttond_default: "test",
+      };
+    let img_array = [
+        "https://placehold.co/400?text=ciao",
+        "https://placehold.co/400?text=bellezze",
+        "https://placehold.co/400?text= trapani in fiamme",
+      ];
+      const[img_show, setImgShow] = useState(0)
 
+    return(
+        <div className="page-background">
+            <PageTitle props = {titolo_info}/>
+            <div className="chng-visualization-button">
+                <button onClick={()=>(setImgShow(0))}> Gallery</button>
+                <button onClick={()=>(setImgShow(1))}> SlideShow</button>
+            </div>
+            {img_show === 1? <ImgSlideShow img_array={img_array}/> : <ImgGallery img_array={img_array}/>}
+
+
+
+        </div>
 
 
     );
--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -2,17 +2,10 @@ import { ImgGallery } from "../../components/ImgGallery/ImgGallery";
 import { ImgSlideShow } from "../../components/ImgSlideShow/ImgSlideShow";
 import "./Home.css";
 export function Home() {
-  let img_array = [
-    "https://placehold.co/400?text=ciao",
-    "https://placehold.co/400?text=bellezze",
-    "https://placehold.co/400?text= trapani in fiamme",
 
-  
-  ];
   return (
     <>
-      <ImgSlideShow img_array={img_array} />
-      <ImgGallery img_array={img_array} />
+
     </>
   );
 }
