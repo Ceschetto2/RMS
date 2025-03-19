a60c3c29100f54bb4bd3faaf678e8df0a8e16ebc
Francesco <francesco.bocci09@gmail.com>
2025-03-19 17:05:18 +0100

    Aggiunta una versione alpha della gallery

---
+++ react/src/components/ImgGallery/ImgGallery.jsx
@@ -0,0 +1,11 @@
+
+import "./ImgGallery.css"
+export function ImgGallery({img_array})
+{
+    return(
+    <div className="gallery-container">
+        {img_array.map((img_source, index)=>(<img className="img-gallery" src ={img_source}/>))}
+        
+    </div>
+    );
+}
--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -1,15 +1,18 @@
+import { ImgGallery } from "../../components/ImgGallery/ImgGallery";
 import { ImgSlideShow } from "../../components/ImgSlideShow/ImgSlideShow";
-import "./Home.css"
-export function Home(){
-      let img_array = [
-        "https://placehold.co/400?text=ciao",
-        "https://placehold.co/400?text=bellezze",
-        "https://placehold.co/400?text= trapani in fiamme",
-      ];
-    return(
-        
-        <>
-          <ImgSlideShow img_array={img_array}/>
-        </>
-    );
+import "./Home.css";
+export function Home() {
+  let img_array = [
+    "https://placehold.co/400?text=ciao",
+    "https://placehold.co/400?text=bellezze",
+    "https://placehold.co/400?text= trapani in fiamme",
+
+  
+  ];
+  return (
+    <>
+      <ImgSlideShow img_array={img_array} />
+      <ImgGallery img_array={img_array} />
+    </>
+  );
 }
