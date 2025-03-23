0af4082cb8146f2833f67e7845e5e5ee72d82039
Francesco <francesco@192.168.1.54>
2025-03-23 18:45:40 +0100

    Svolgendo test con sequelize per le query... risultati deludenti dovuti alla complessa sintassi.

--- backend-serv/routes/GalleryImages_routes.js
+++ backend-serv/routes/GalleryImages_routes.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const {GalleryImage} = require("../models")
+const express = require("express");
+const router = express.Router();
+const { GalleryImage, sequelize } = require("../models");
 
 
 
-router.get("/",async(req, res ) =>{
+router.get("/", async (req, res) => {
+  //const listOfImages = await GalleryImage.findAll();
 
-const listOfImages= await GalleryImage.findAll();
-res.json(listOfImages);
-})
+  const [listOfImages] = await sequelize.query("SELECT *.* FROM GalleryImages WHERE id ="+2, {
+    type: sequelize.QueryTypes.SELECT
+  });
+  console.log(listOfImages)
+  res.json(listOfImages);
+});
 
+router.post("/", async (req, res) => {
+  const imgs = req.body;
 
+  await GalleryImage.bulkCreate(imgs);
+  res.json("Inserimento avvenuto con successo");
+});
 
-
-
-module.exports = router
+module.exports = router;
