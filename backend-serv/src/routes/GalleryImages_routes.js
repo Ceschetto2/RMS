576686eaf0812b54ac8d3a31367cc6515c80d811
Francesco <francesco.bocci09@gmail.com>
2025-03-24 17:58:42 +0100

    Fixato il collegamento backend-frontend per quanto riguarda le immagini della gallery

--- backend-serv/src/routes/GalleryImages_routes.js
+++ backend-serv/src/routes/GalleryImages_routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const GalleryController = require('../controllers/gallery_controllers/galleryController');
 
-
+//Nelle routes verranno inserite tutte le rotte riguardanti una determinata componente
 router.get("/", GalleryController.getImages);
 router.post("/", GalleryController.sendImages);
 //Possiamo anche specializzare la route per legarla a diverse funzioni o query. In questo caso questa routes fa la stessa cosa della prima
