4868895e81cd1bb37ae4e62921966a33055624ab
Francesco <francesco.bocci09@gmail.com>
2025-03-25 16:10:19 +0100

    aggiunta la possibilità di viusalizzare in fullscreen una immagine nellla ImgGallery

--- react/src/components/ImgGallery/ImgGallery.jsx
+++ react/src/components/ImgGallery/ImgGallery.jsx
@@ -1,11 +1,26 @@
-
-import "./ImgGallery.css"
-export function ImgGallery({img_array})
-{
-    return(
+import { useState } from "react";
+import "./ImgGallery.css";
+export function ImgGallery({ img_array }) {
+    const[fullScreenImage, setFullScreenImage] = useState(null);
+  return (
     <div className="gallery-container">
-        {img_array.map((img_source, index)=>(<img className="img-gallery" src ={img_source.img_link}/>))}
-        
+      {img_array.map((img_source, index) => (
+        <button className="button-img" onClick={() => setFullScreenImage(img_source.img_link)}>
+          <img key={index}  className="img-gallery" src={img_source.img_link} />
+        </button>
+        ))}
+
+        {fullScreenImage === null? null: renderFullScreenImage(fullScreenImage)}
     </div>
+  );
+
+  function renderFullScreenImage() {
+    console.log("kitemurt")
+    return (
+      <div className="test" onClick={()=>setFullScreenImage(null)}>
+        <img className="full-img" src={fullScreenImage} />
+      </div>
     );
+  }
 }
+0
