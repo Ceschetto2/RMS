9d9e42f7e213c85cb0806b2a6e741205b6d499ab
francesco <francesco.bocci09@gmail.com>
2025-05-20 19:10:48 +0200

    Refactor dei className del componente AdviseContainer perchè contrastavano il ContainerInfo

--- react/src/components/ContainerInfo/ContainerInfo.jsx
+++ react/src/components/ContainerInfo/ContainerInfo.jsx
@@ -9,27 +9,26 @@ Il componente ContainerInfo rappresenta un contenitore per visualizzare informaz
 - Lo stile del componente è gestito tramite il file CSS "ContainerInfo.css".
 */
 
-export function ContainerInfo({img_url, title, object, description, createdAt})
-{
+export function ContainerInfo({ img_url, title, object, description, createdAt }) {
     if (!(title || object || description)) return null;
-    return(
+    return (
         <div className="container-info">
-            <img className="immagine-info" src={img_url}  alt ="Immagine mancante"></img>
+            <img className="immagine-info" src={img_url} alt="Immagine mancante"></img>
             <div className="text-box">
                 <div className="header-info-box">
-                    <text className="titolo-info" alt = "titolo"> {title} </text>
-                    <text className="data-pubblicazione-info" alt = "nan "> data pubblicazione: {transformDate(createdAt)}  </text>
+                    <text className="titolo-info" alt="titolo"> {title} </text>
+                    <text className="data-pubblicazione-info" alt="nan "> data pubblicazione: {transformDate(createdAt)}  </text>
 
                 </div>
-                <text className="oggetto-info" alt = "oggetto"> {object}  </text>    
-                <text className="descrizione-info" alt = "descrizione "> {description}  </text>
+                <text className="oggetto-info" alt="oggetto"> {object}  </text>
+                <text className="descrizione-info" alt="descrizione "> {description}  </text>
             </div>
         </div>
     );
 }
 
-function transformDate(dataString){
+function transformDate(dataString) {
     return new Date("2025-03-26T11:56:07.000Z").toLocaleDateString();
-   
+
 
 }
