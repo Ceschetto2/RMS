796c541a78420e86610b64f0e45362b5f30c7a6d
Francesco <francesco.bocci09@gmail.com>
2025-05-26 09:38:35 +0200

    risolto conflitto di classi CSS per i componenti advise Container e ContainerInfo

--- react/src/components/AdviseContainer/AdviseContainer.jsx
+++ react/src/components/AdviseContainer/AdviseContainer.jsx
@@ -1,24 +1,23 @@
 import "./AdviseContainer.css"
-import {UseState} from "react";
+import { UseState } from "react";
 import StandardNewsImage from "../../Assets/StandardNewsImage.jpg";
 
 
-export function AdviseShow({img_url,title,object,description}){
+export function AdviseShow({ img_url, title, object, description }) {
     if (!(title || object || description)) return null;
-    return(
-    
+    return (
+
         <div className="adv-show">
-             <img className="immagine-info" src={img_url||StandardNewsImage }  alt ="Immagine mancante"></img>
-            
-        
-                 <text className="titolo-info" alt = "titolo"> {title} </text>
-              {/*<div className="text-box"> </div>
-                <div className="text-box"></div>*/}
-                 <text className="oggetto-info" alt = "oggetto"> {object}  </text>    
-             <text className="descrizione-info" alt = "descrizione "> {description}  </text>
-           
-    </div>
-        
+            <img className="adv-immagine-info" src={img_url || StandardNewsImage} alt="Immagine mancante"></img>
+
+
+            <text className="adv-titolo-info" alt="titolo"> {title} </text>
+
+            <text className="adv-oggetto-info" alt="oggetto"> {object}  </text>
+            <text className="adv-descrizione-info" alt="descrizione "> {description}  </text>
+
+        </div>
+
     )
 
 
